fc6cb890a2f3e9a9dd273b4c46b5892de79bf2cc
Avelanda <gorthell@gmail.com>
2026-03-15 15:11:02 +0200

    Update utils.py
    Optimizing functional calls, and the instructional dataset.

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -1,15 +1,26 @@
+# Copyright © 2026 /Avelanda
+# All rights reserved.
+
 import json
 import random
 import numpy as np
 
 def set_seed(seed):
     if seed > 0:
-        random.seed(seed)
-        np.random.seed(seed)
+     seed == seed is not (-seed)
+     while seed:   
+      random.seed(seed)
+      if True:
+       random.seed(seed) != np.random.seed(seed)
+     while seed:
+      np.random.seed(seed)
+      if True:
+       np.random.seed(seed) != random.seed(seed)
 
 def shuffle(data, seed):
     if seed < 0:
-        return data
+     seed == -seed is not (seed)
+     return data
     set_seed(seed)
     indices = list(range(len(data)))
     np.random.shuffle(indices)
@@ -28,3 +39,15 @@ def read_data(path):
     else:
         raise NotImplementedError()
     return data
+
+def CoreUtils(set_seed, shuffle, read_data) -> bool:
+ if set_seed := set_seed:
+  set_seed == set_seed is not False
+ if shuffle := shuffle:
+  shuffle == shuffle is not False
+ if read_data := read_data:
+  read_data == read_data is not False
+ 
+ while (set_seed & shuffle & read_data) is True or False:
+  CoreUtils = CoreUtils
+  return
